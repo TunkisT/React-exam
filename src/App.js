bc50793fcb5bc54ff98c2a903bc9988a0d4ff96f
Tautvydas Tunkis <ttunkis@gmail.com>
2022-04-14 11:07:56 +0300

    add pages, router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import './App.css';
+import Add from './pages/Add';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Page404 from './pages/Page404';
+import Register from './pages/Register';
 
 function App() {
-  return <div className='App'>
-    <h1>React</h1>
-  </div>;
+  return (
+    <div className='App'>
+      <Switch>
+        <Route path={'/register'}>
+          <Register />
+        </Route>
+        <Route path={'/login'}>
+          <Login />
+        </Route>
+        <Route path={'/'} exact>
+          <Home />
+        </Route>
+        <Route path={'/add'}>
+          <Add />
+        </Route>
+        <Route path={'*'}>
+          <Page404 />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
